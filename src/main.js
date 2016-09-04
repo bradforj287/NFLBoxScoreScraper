880132a5e7b906aa29688b4925e42f532da89f8a
Brad Jones <brjones@akamai.com>
2016-09-04 09:08:21 -0400

    refactoring

---
+++ src/main.js
@@ -0,0 +1,193 @@
+var fs = require('fs');
+var async = require("async");
+
+var nflScraper = require('./scraper/pro-football-ref-scraper.js');
+var nflUtils = require('./utils/nfl-utils.js');
+
+
+var programArgs = JSON.parse(fs.readFileSync('./arguments.json', 'utf8'));
+var firstYear = programArgs.minNflYear;
+var lastYear = programArgs.maxNflYear;
+var nflRootDir = programArgs.nflDataRoot;
+
+module.exports = fs.existsSync || function existsSync(filePath) {
+        try {
+            fs.statSync(filePath);
+        } catch (err) {
+            if (err.code == 'ENOENT') return false;
+        }
+        return true;
+    };
+
+var NUM_PARALLEL_REQ = 1;
+
+function chunkArray(arr, chunkSize) {
+    var array = arr;
+    return [].concat.apply([],
+        array.map(function (elem, i) {
+            return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
+        })
+    );
+}
+
+var playerLinksSeen = {};
+
+function recordPlayerLinks(bs) {
+    var playerStatsList = bs.playerStatsList;
+    playerStatsList.forEach(function (ps) {
+        var link = ps.playerLink;
+        playerLinksSeen[link] = true;
+    });
+}
+
+function getAllPlayerLinksSeen() {
+    var keys = [];
+    for (var key in playerLinksSeen) {
+        if (playerLinksSeen.hasOwnProperty(key)) {
+            keys.push(key);
+        }
+    }
+    return keys;
+}
+
+function toJSON(obj) {
+    return JSON.stringify(obj, null, 2)
+}
+
+var mkdirSync = function (path) {
+    try {
+        fs.mkdirSync(path);
+    } catch (e) {
+        if (e.code != 'EEXIST') throw e;
+    }
+};
+
+function scrapeBoxScoreChunk(summaries, onFinishFunc) {
+    async.each(summaries,
+        function (item, callback) {
+            var week = item.week;
+            var wTeam = nflUtils.getTeamShortName(item.winningTeam);
+            var lTeam = nflUtils.getTeamShortName(item.losingTeam);
+            var fileName = "week_" + week + "_" + wTeam + "_" + lTeam + ".json";
+            var filePath = nflRootDir + "/" + item.seasonYear + "/box_scores/" + fileName;
+            if (fs.existsSync(filePath)) {
+                console.log('NO NEED to scrape box score for: ' + item.boxScoreLink + " -> " + nflUtils.getGameTitleForSummary(item));
+                var theData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+                recordPlayerLinks(theData);
+                callback();
+            } else {
+                nflScraper.scrapeBoxScore(item, function (data) {
+                    recordPlayerLinks(data);
+                    if (data.playerStatsList.length > 0) {
+                        fs.writeFile(filePath, toJSON(data), function (err) {
+                            console.log(filePath);
+                        });
+                    }
+                    callback();
+                });
+            }
+        },
+        function (err) {
+            onFinishFunc();
+        }
+    );
+}
+
+function scrapeBoxScores(summaries, onFinishFunc) {
+    var chunkedArray = chunkArray(summaries, NUM_PARALLEL_REQ);
+
+    async.eachSeries(chunkedArray,
+        function (item, callback) {
+            scrapeBoxScoreChunk(item, callback);
+        },
+        function (err) {
+            onFinishFunc();
+        }
+    );
+}
+
+function ensureDirs(year) {
+    mkdirSync(nflRootDir);
+    mkdirSync(nflRootDir + "/players");
+    mkdirSync(nflRootDir + "/" + year);
+    mkdirSync(nflRootDir + "/" + year + "/box_scores");
+}
+
+function scrapeNflYear(year, finishedFunc) {
+    console.log('scraping nfl year ' + year);
+    console.log('-----------------------\n');
+    ensureDirs(year);
+    console.log('scraping game summaries');
+    nflScraper.scrapeGameSummaries(year, function (data, isSuccess) {
+        if (!isSuccess) {
+            console.log("error scraping summaries! exiting");
+            return;
+        }
+        var filePath = nflRootDir + "/" + year + "/game_summaries.json";
+        fs.writeFile(filePath, toJSON(data), function (err) {
+            console.log('wrote ' + filePath);
+        });
+
+        scrapeBoxScores(data, finishedFunc);
+    });
+}
+
+function scrapePlayerInfoChunk(list, cb) {
+    async.each(list,
+        function (item, callback) {
+            var fileName = item.replace(/\//g, "-") + ".json";
+            var filePath = nflRootDir + "/players/" + fileName;
+
+            if (fs.existsSync(filePath)) {
+                console.log('NOT scraping player_info: ' + item + ' already exists');
+                callback();
+            } else {
+                console.log('scraping player_info: ' + item);
+                nflScraper.scrapePlayerInfo(item, function (data) {
+
+                    fs.writeFile(filePath, toJSON(data), function (err) {
+                        console.log('wrote ' + filePath);
+                    });
+                    callback();
+                });
+            }
+        },
+        function (err) {
+            cb();
+        }
+    );
+}
+
+function scrapeAllPlayerInfo(cb) {
+    var scrapeList = getAllPlayerLinksSeen();
+
+    var chunkedArray = chunkArray(scrapeList, NUM_PARALLEL_REQ);
+    var chunk = 1;
+    async.eachSeries(chunkedArray,
+        function (item, callback) {
+            console.log("** Scraping chunk " + chunk + "/" + chunkedArray.length);
+            scrapePlayerInfoChunk(item, callback);
+            chunk++;
+        },
+        function (err) {
+            console.log("Finished scraping player info");
+        }
+    );
+}
+
+var years = [];
+
+for (var i = firstYear; i <= lastYear; i++) {
+    years.push(i);
+}
+
+
+async.eachSeries(years,
+    function (item, callback) {
+        scrapeNflYear(item, callback);
+    },
+    function (err) {
+        console.log("finished scraping NFL years");
+        scrapeAllPlayerInfo();
+    }
+);
---
+++ src/utils/nfl-utils.js
@@ -0,0 +1,50 @@
+var exports = module.exports = {};
+
+const teamNameToShortHand = {
+    "Seattle Seahawks": "SEA",
+    "Green Bay Packers": "GNB",
+    "Denver Broncos": "DEN",
+    "Indianapolis Colts": "IND",
+    "Carolina Panthers": "CAR",
+    "Tampa Bay Buccaneers": "TAM",
+    "Pittsburgh Steelers": "PIT",
+    "Cleveland Browns": "CLE",
+    "New York Jets": "NYJ",
+    "Oakland Raiders": "OAK",
+    "Cincinnati Bengals": "CIN",
+    "Baltimore Ravens": "BAL",
+    "Miami Dolphins": "MIA",
+    "New England Patriots": "NWE",
+    "Houston Texans": "HOU",
+    "Washington Redskins": "WAS",
+    "Atlanta Falcons": "ATL",
+    "New Orleans Saints": "NOR",
+    "San Francisco 49ers": "SFO",
+    "Dallas Cowboys": "DAL",
+    "Philadelphia Eagles": "PHI",
+    "Jacksonville Jaguars": "JAX",
+    "Tennessee Titans": "TEN",
+    "Tennessee Oilers": "TEN",
+    "Kansas City Chiefs": "KAN",
+    "Minnesota Vikings": "MIN",
+    "St. Louis Rams": "STL",
+    "Buffalo Bills": "BUF",
+    "Chicago Bears": "CHI",
+    "Arizona Cardinals": "ARI",
+    "San Diego Chargers": "SDG",
+    "Detroit Lions": "DET",
+    "New York Giants": "NYG"
+};
+
+
+exports.getGameTitleForSummary = function(summary) {
+    var spacing = " ";
+    if (summary.isWinningTeamHome) {
+        spacing = " @ ";
+    }
+    return summary.winningTeam + spacing + summary.losingTeam;
+};
+
+exports.getTeamShortName = function(fullName) {
+    return teamNameToShortHand[fullName];
+};
