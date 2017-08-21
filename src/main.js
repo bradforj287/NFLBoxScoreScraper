67565b3998abfd1ac5a49994a00578a661898ff2
Brad Jones <brjones@akamai.com>
2017-08-20 20:53:53 -0400

    fix

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ module.exports = fs.existsSync || function existsSync(filePath) {
     try {
         fs.statSync(filePath);
     } catch (err) {
-        if (err.code == 'ENOENT') return false;
+        if (err.code === 'ENOENT') return false;
     }
     return true;
 };
